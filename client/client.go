66fc57a522094aa0d2fb778d13c129b38fc26f1e
Karim Sonbol <karim.sonbol279@gmail.com>
2020-01-12 18:50:34 +0300

    refactor client to use as a library

--- client/client.go
+++ client/client.go
@@ -8,135 +8,49 @@ import (
 	"log"
 	"time"
 
-	// "google.golang.org/grpc/codes"
-	// "google.golang.org/grpc/status"
-
+	pb "edgekv/frontend/frontend"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/testdata"
-
-	pb "edgekv/frontend/frontend"
 )
 
 var (
+	serverAddr         = flag.String("server_addr", "localhost:2381", "The server address in the format of host:port")
 	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
-	serverAddr         = flag.String("server_addr", "localhost:10000", "The server address in the format of host:port")
-	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name use to verify the hostname returned by TLS handshake")
+	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name used to verify the hostname returned by TLS handshake")
 )
 
-// Get the value associated with a key and data type from kv store
-func Get(client pb.FrontendClient, req *pb.GetRequest) (*pb.GetResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	res, err := client.Get(ctx, req)
-	return res, err
+// Client is the user endpoint access to edgekv
+type Client interface {
+	// Close the client gRPC connection
+	Close() error
+
+	// Get the value associated with a key and data type from kv store
+	Get(key string, dataType bool) (string, error)
+
+	// Put adds the key-value pair or updates the value of given key
+	Put(key string, dataType bool, value string) error
+
+	// Del delete key from server's key-value store
+	Del(key string, dataType bool) error
 }
 
-// Put Add a new key-value pair
-func Put(client pb.FrontendClient, req *pb.PutRequest) (*pb.PutResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	res, err := client.Put(ctx, req)
-	return res, err
-	// if err != nil {
-	// log.Fatalf("%v.Put(_) = _, %v: ", client, err)
-	// }
-	// return res, nil
+// EdgekvClient is the client of edgekv key-value store
+type EdgekvClient struct {
+	conn       *grpc.ClientConn
+	rpcClient  pb.FrontendClient
+	rpcTimeout time.Duration
 }
 
-// Del delete key from server's key-value store
-func Del(client pb.FrontendClient, req *pb.DeleteRequest) (*pb.DeleteResponse, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	res, err := client.Del(ctx, req)
-	if err != nil {
-		log.Fatalf("%v.Del(_) = _, %v: ", client, err)
-	}
-	return res, nil
-}
-
-// Functions to use in the benchmark
-
-// get just send the value or error to stdout
-// TODO: return value instead and use it in benchmark
-func get(client pb.FrontendClient, dataT bool, key string) (string, error) {
-	var val string
-	var returnErr error
-	getRes, err := Get(client, &pb.GetRequest{Key: key, Type: dataT})
-	// return getRes.GetValue(), err
-	if err != nil {
-		returnErr = fmt.Errorf("get operation failed: %v", err)
-		// if st, ok := status.FromError(err); ok {
-		// 	code := st.Code()
-		// 	if code == codes.NotFound {
-		// 		returnErr = fmt.Errorf("get operation failed: %v", err)
-		// 	}
-		// } else {
-		// 	returnErr = fmt.Errorf(err)
-		// }
-	} else {
-		// log.Printf("Value: %s, Size: %d\n", getRes.GetValue(), getRes.GetSize())
-		val = getRes.GetValue()
-	}
-	// fmt.Printf("Value: %s, Size: %d\n", getRes.GetValue(), getRes.GetSize())
-	return val, returnErr
-}
-
-// put just prints the output or error to stdout
-// TODO: return value instead and use it in benchmark
-func put(client pb.FrontendClient, dataT bool, key string, val string) (int32, error) {
-	var returnSt int32 // return status
-	var returnErr error
-	putRes, err := Put(client, &pb.PutRequest{Key: key, Type: dataT, Value: val})
-	returnSt = putRes.GetStatus()
-	returnErr = err
-	if err != nil {
-		returnErr = fmt.Errorf("Put operation failed: %v", err)
-	} else {
-		switch returnSt {
-		case utils.KVAddedOrUpdated:
-			// log.Println("Put operation completed successfully.")
-		case utils.UnknownError:
-			// log.Printf("Put operation failed: %v.\n", err)
-			returnErr = fmt.Errorf("Unkown error happened, put operation failed")
-
-		}
-	}
-	return returnSt, returnErr
-}
-
-// put just prints the output or error to stdout
-// TODO: return value instead and use it in benchmark
-func del(client pb.FrontendClient, dataT bool, key string) (int32, error) {
-	var returnSt int32
-	var returnErr error
-	delRes, err := Del(client, &pb.DeleteRequest{Key: key, Type: dataT})
-	returnSt = delRes.GetStatus()
-	returnErr = err
-	if err != nil {
-		// log.Printf("Delete operation failed: %v", err)
-	} else {
-		switch returnSt {
-		case utils.KVDeleted:
-			// log.Println("Delete operation completed successfully.")
-		case utils.KeyNotFound:
-			returnErr = fmt.Errorf("delete failed: could not find key: %s", key)
-		case utils.UnknownError:
-			returnErr = fmt.Errorf("delete operation failed: %v", err)
-		}
-	}
-	return returnSt, returnErr
-}
-
-func getConnectionToServer() *grpc.ClientConn {
-	flag.Parse()
+// NewEdgekvClient creates an edgekv client with server address
+func NewEdgekvClient(serverAddr string, tls bool, caFile string,
+	serverHostOverride string) (*EdgekvClient, error) {
 	var opts []grpc.DialOption
-	if *tls {
-		if *caFile == "" {
-			*caFile = testdata.Path("ca.pem")
-		}
-		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+	if tls {
+		// if caFile == "" {
+		// 	set default caFile path
+		// }
+		creds, err := credentials.NewClientTLSFromFile(caFile, serverHostOverride)
 		if err != nil {
 			log.Fatalf("Failed to create TLS credentials %v", err)
 		}
@@ -145,22 +59,75 @@ func getConnectionToServer() *grpc.ClientConn {
 		opts = append(opts, grpc.WithInsecure())
 	}
 	opts = append(opts, grpc.WithBlock())
-	conn, err := grpc.Dial(*serverAddr, opts...)
+	conn, err := grpc.Dial(serverAddr, opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
-	return conn
+	client := pb.NewFrontendClient(conn)
+	return &EdgekvClient{conn: conn, rpcClient: client, rpcTimeout: 10 * time.Second}, err
 }
 
-// run with flag -server_addr=localhost:PORT, default is localhost:2379
-func main() {
-	conn := getConnectionToServer()
-	defer conn.Close()
-	client := pb.NewFrontendClient(conn)
+// InsecureEdgekvClient returns a client connected without credentials
+// Do not use in production!
+func NewInsecureEdgekvClient(serverAddr string) (*EdgekvClient, error) {
+	return NewEdgekvClient(serverAddr, false, "", "")
+}
 
-	fmt.Println(get(client, utils.LocalData, "10"))
-	fmt.Println(put(client, utils.LocalData, "10", "val"))
-	fmt.Println(get(client, utils.LocalData, "10"))
-	fmt.Println(del(client, utils.LocalData, "10"))
-	fmt.Println(get(client, utils.LocalData, "10"))
+// Close the client gRPC connection
+func (c *EdgekvClient) Close() error {
+	c.conn.Close()
+	return nil
+}
+
+// Get the value associated with a key and data type from kv store
+func (c *EdgekvClient) Get(key string, dataType bool) (string, error) {
+	// TODO: should we change this timeout?
+	ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)
+	defer cancel()
+	req := &pb.GetRequest{Key: key, Type: dataType}
+	res, err := c.rpcClient.Get(ctx, req)
+	return res.GetValue(), err
+}
+
+// Put adds the key-value pair or updates the value of given key
+func (c *EdgekvClient) Put(key string, dataType bool, value string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)
+	defer cancel()
+	req := &pb.PutRequest{Key: key, Type: dataType, Value: value}
+	_, err := c.rpcClient.Put(ctx, req)
+	return err
+}
+
+// Del removes the key-value pair from kv store
+func (c *EdgekvClient) Del(key string, dataType bool) error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)
+	defer cancel()
+	req := &pb.DeleteRequest{Key: key, Type: dataType}
+	res, err := c.rpcClient.Del(ctx, req)
+	st := res.GetStatus()
+	returnErr := err
+	if err == nil {
+		switch st {
+		case utils.KeyNotFound:
+			returnErr = fmt.Errorf("delete failed: could not find key: %s", key)
+		case utils.UnknownError:
+			returnErr = fmt.Errorf("delete operation failed: %v", err)
+		}
+	}
+	return returnErr
+}
+
+// run with flag -server_addr=localhost:PORT, default is localhost:2381
+func main() {
+	flag.Parse()
+	cl, err := NewEdgekvClient(*serverAddr, *tls, *caFile, *serverHostOverride)
+	if err != nil {
+		fmt.Printf("Error while creating edgekv client %v\n", err)
+	}
+	key := "key"
+	fmt.Println(cl.Get(key, utils.LocalData))
+	fmt.Println(cl.Put(key, utils.LocalData, "val"))
+	fmt.Println(cl.Get(key, utils.LocalData))
+	fmt.Println(cl.Del(key, utils.LocalData))
+	fmt.Println(cl.Get(key, utils.LocalData))
 }
