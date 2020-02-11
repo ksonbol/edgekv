a7747918274a04c4b308685ccf58f3e390c89dec
Karim Sonbol <karim.sonbol279@gmail.com>
2020-02-11 21:41:28 +0300

    copy needed keys after join or predecessor change

--- pkg/dht/node.go
+++ pkg/dht/node.go
@@ -30,6 +30,12 @@ type Config struct {
 
 var defaultConfig *Config = &Config{IDBits: 160, IDChars: 40, IDFunc: GenID}
 
+const (
+	created = 0
+	joined  = 1
+	ready   = 2
+)
+
 // var defaultConfig *Config = &Config{IDBits: 5, IDChars: 3, idFunc: shortID}
 
 // Node represents a node in the DHT overlay
@@ -48,6 +54,8 @@ type Node struct {
 	Conf       *Config
 	storage    Storage
 	leaving    bool // set true when node is leaving the ring
+	state      int
+	stateMut   sync.RWMutex
 }
 
 // newBasicNode creates a node with the essential parameters
@@ -76,6 +84,7 @@ func NewLocalNode(addr string, st Storage, conf *Config) *Node {
 	n.server.RunInsecure()
 	fmt.Printf("node %s: Server is running", addr)
 	n.Transport = newTransport(n, nil, nil)
+	n.setState(created)
 	return n
 }
 
@@ -117,11 +126,10 @@ func (n *Node) Join(helperNode *Node) error {
 
 		n.SetSuccessor(succ)
 	}
+	n.setState(joined)
 	go n.stabilize()
 	go n.fixFingers()
 	go n.checkPredecessor()
-	time.Sleep(3 * time.Second) // wait for stabilization
-	n.getFirstSnapshot()
 	return nil
 }
 
@@ -183,6 +191,9 @@ func (n *Node) IsLeavingRPC() (bool, error) {
 
 // GetKV gets the KV from the connected edge group or from a remote group
 func (n *Node) GetKV(key string) (string, error) {
+	if n.getState() < ready {
+		return "", fmt.Errorf("request failed, node not ready yet")
+	}
 	var err error
 	if n.CanStore(key) {
 		var val string
@@ -200,6 +211,9 @@ func (n *Node) GetKV(key string) (string, error) {
 
 // PutKV puts the KV to the connected edge group or to a remote group
 func (n *Node) PutKV(key, value string) error {
+	if n.getState() < ready {
+		return fmt.Errorf("request failed, node not ready yet")
+	}
 	var succ *Node
 	var err error
 	if n.CanStore(key) {
@@ -214,6 +228,10 @@ func (n *Node) PutKV(key, value string) error {
 // putKVLocal stores the kv-pair in the connected group if it is responsible for it
 // otherwise, it deos nothing and returns an error (does not send to other remote groups)
 func (n *Node) putKVLocal(key, value string) error {
+	if n.getState() < joined {
+		// need to join dht first to stabilize and update predecessor
+		return fmt.Errorf("request failed, node not ready yet")
+	}
 	if n.CanStore(key) {
 		return n.storage.PutKV(key, value)
 	}
@@ -222,6 +240,9 @@ func (n *Node) putKVLocal(key, value string) error {
 
 // DelKV removes the KV from the connected edge group or from a remote group
 func (n *Node) DelKV(key string) error {
+	if n.getState() < ready {
+		return fmt.Errorf("request failed, node not ready yet")
+	}
 	var succ *Node
 	var err error
 	if n.CanStore(key) {
@@ -235,6 +256,9 @@ func (n *Node) DelKV(key string) error {
 
 // RangeGetKV returns kv-pairs in specified range from connected edge group
 func (n *Node) RangeGetKV(start, end string) (map[string]string, error) {
+	if n.getState() < ready {
+		return nil, fmt.Errorf("request failed, node not ready yet")
+	}
 	return n.storage.RangeGetKV(start, end)
 }
 
@@ -302,14 +326,19 @@ func (n *Node) stabilize() {
 				}
 			}
 			if newSucc.ID != succ.ID {
-				// if n == successor OR newSucc in (n, successor)
+				// if n == successor (only node in ring) OR newSucc in (n, successor)
 				if (n == succ) || (inInterval(newSucc.ID, incID(n.ID, n.Conf.IDChars), succ.ID)) {
 					log.Printf("Replacing node %s old successor (%s) with %s\n",
 						n.ID, succ.ID, newSucc.ID)
 					n.SetSuccessor(newSucc)
 				}
 			}
-			succ = n.Successor() // get the possibly updated successor
+			succ = n.Successor()        // get the possibly updated successor
+			if n.getState() == joined { // get keys n is responsible for
+				// TODO: should i copy all the keys before notifying successor to make sure no keys are deleted from them
+				// or should i do it in another goroutine to make dht be stable more quickly?
+				go n.loadFirstSnapshot()
+			}
 			if n != succ {
 				succ.NotifyRPC(n)
 			}
@@ -347,7 +376,7 @@ func (n *Node) fixFingers() {
 
 // fixFingers periodically checks if predecessor is available or leaving the network
 func (n *Node) checkPredecessor() {
-	var pred *Node
+	var pred, newPred *Node
 	var err error
 	var leaving bool
 	if n == n.Successor() {
@@ -366,11 +395,16 @@ func (n *Node) checkPredecessor() {
 				log.Fatalf("Error while communicating with predecessor %v", err)
 			}
 			if leaving {
-				pred, err = pred.GetPredecessorRPC()
+				newPred, err = pred.GetPredecessorRPC()
 				if err != nil {
 					log.Fatalf("Error while communicating with predecessor %v", err)
 				}
-				n.SetPredecessor(pred)
+				n.SetPredecessor(newPred)
+				// get all keys of leaving node, since they are now responsiblity of n!
+				// do that in another go routine since it may take some time to finish
+				// get all keys in range (newPred, leavingPred] from leavingPred
+				// TODO: get and load keys using a snaphot instead of a range query
+				go n.rangeGetAndPutKV(pred, incID(newPred.ID, n.Conf.IDChars), incID(pred.ID, n.Conf.IDChars))
 			}
 		case <-n.shutdownCh:
 			ticker.Stop()
@@ -379,25 +413,36 @@ func (n *Node) checkPredecessor() {
 	}
 }
 
-func (n *Node) getFirstSnapshot() {
+// gets keys of n's responsibility from n's successor
+// should be called once during join process
+func (n *Node) loadFirstSnapshot() {
 	succ := n.Successor()
-	if n == succ {
-		<-n.nodeJoinCh // if only node, wait until other nodes join
+	// succ has (succ.pred, succ], n asks for (succ, n]
+	// n will get the intersection which is (succ.pred, n]
+	n.rangeGetAndPutKV(succ, incID(succ.ID, n.Conf.IDChars), incID(n.ID, n.Conf.IDChars))
+	n.setState(ready)
+}
+
+// rangeGetAndPutKV gets keys in range [start, end) from remote node and writes them to local edge group
+func (n *Node) rangeGetAndPutKV(remote *Node, start string, end string) {
+	if n.getState() < joined {
+		// need to join dht first to stabilize and update predecessor
+		log.Fatalf("request failed, node not ready yet")
 	}
-	// get keys in range (predecessor, n.ID]
-	kvs, err := succ.RangeGetKVRPC(incID(n.Predecessor().ID, n.Conf.IDChars), incID(n.ID, n.Conf.IDChars))
+	kvs, err := remote.RangeGetKVRPC(start, end)
 	if err != nil {
-		log.Fatalf("Node initialization failed: could not copy keys from successor")
+		log.Fatalf("Could not copy keys from node %s: %v", remote.ID, err)
 	}
-	for k, v := range kvs {
-		if err := n.putKVLocal(k, v); err != nil {
-			log.Fatalf("Failed to add key to connected edge group %v", err)
-		}
+	if err = n.multiPutKV(kvs); err != nil {
+		log.Fatalf("Failed to add keys to node: %v", err)
 	}
 }
 
 // findSuccessor finds first node that follows ID
 func (n *Node) findSuccessor(ID string) (*Node, error) {
+	if n.getState() < joined {
+		return nil, fmt.Errorf("node did not join the DHT ring yet, try again later")
+	}
 	pred, err := n.findPredecessor(ID)
 	if err != nil {
 		return nil, err
@@ -469,6 +514,15 @@ func (n *Node) GetFTID(idx int) string {
 	return n.ft[idx].node.ID
 }
 
+func (n *Node) multiPutKV(kvs map[string]string) error {
+	for k, v := range kvs {
+		if err := n.putKVLocal(k, v); err != nil {
+			log.Fatalf("Failed to add key %s to node: %v", k, err)
+		}
+	}
+	return nil
+}
+
 // Leave the DHT ring and stop the node
 func (n *Node) Leave() error {
 	n.setLeaving()
@@ -515,6 +569,18 @@ func (n *Node) ZeroID() string {
 // MaxID return the maximum possible ID in the ring
 func (n *Node) MaxID() string {
 	return appendZeros("f", n.Conf.IDChars)
+}
+
+func (n *Node) getState() int {
+	n.stateMut.RLock()
+	defer n.stateMut.RUnlock()
+	return n.state
+}
+
+func (n *Node) setState(st int) {
+	n.stateMut.Lock()
+	defer n.stateMut.Unlock()
+	n.state = st
 }
 
 // GenID creates a sha1 hash of a given string
